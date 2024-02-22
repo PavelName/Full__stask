7272fd4c36e22da4aceab18232d2a06b1c115c76
Pavel Koryagin <pavel.koryagin.76@bk.ru>
2024-02-22 08:58:50 +0800

    untrackted

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-console.log('Winter came ' + 'for the' + ' House of Frey.');
-console.log(String.fromCharCode(126));
-console.log(String.fromCharCode(94));
-console.log(String.fromCharCode(37));
+// console.log('Winter came ' + 'for the' + ' House of Frey.');
+// console.log(String.fromCharCode(126));
+// console.log(String.fromCharCode(94));
+// console.log(String.fromCharCode(37));
+
+let info = "We couldn't verify your mother's maiden name.";
+let intro = 'Here is important information about your account security.';
+
+let firstName = 'Joffrey';
+let greeting = 'Hello';
+
+console.log(greeting + ', ' + firstName +'!');
+console.log(intro +'\n'+info);
