5385cad065c1db898ae0aecdf158cd4dfe22f326
Pavel Koryagin <pavel.koryagin.76@bk.ru>
2024-03-01 17:30:47 +0800

    js

--- script.js
+++ script.js
@@ -3,11 +3,25 @@
 // console.log(String.fromCharCode(94));
 // console.log(String.fromCharCode(37));
 
-let info = "We couldn't verify your mother's maiden name.";
-let intro = 'Here is important information about your account security.';
+// let king = 'King Balon the 6th';
+// let newKing = 6;
+// let allCastle = 17;
+// let result = newKing * allCastle;
+// console.log(`${king} has ${result} rooms.`);
 
-let firstName = 'Joffrey';
-let greeting = 'Hello';
+//const name = 'Na\nharis';
+//console.log(name[name.length - 1]);
 
-console.log(greeting + ', ' + firstName +'!');
-console.log(intro +'\n'+info);
+// const one = 'Naharis';
+// const two = 'Mormont';
+// const three = 'Sand';
+// console.log(`${one[2]}${two[1]}${three[3]}${two[4]}${two[2]}`);
+
+const company1 = 'Apple';
+const company2 = 'Samsung';
+//const companyLength = length(company1);
+
+//console.log(companyLength);
+console.log(company1.length + company2.length);
+//console.log(`${company1.length}` '+' `${company2.length}`);
+ 
