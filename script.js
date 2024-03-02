9e50776052858354367a00c0c69ef0a8311d9263
Pavel Koryagin <pavel.koryagin.76@bk.ru>
2024-03-02 20:57:43 +0800

    learning cycle

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
-// console.log('Winter came ' + 'for the' + ' House of Frey.');
-// console.log(String.fromCharCode(126));
-// console.log(String.fromCharCode(94));
-// console.log(String.fromCharCode(37));
 
-// let king = 'King Balon the 6th';
-// let newKing = 6;
-// let allCastle = 17;
-// let result = newKing * allCastle;
-// console.log(`${king} has ${result} rooms.`);
+//const age = prompt('how areyou age?');
 
-//const name = 'Na\nharis';
-//console.log(name[name.length - 1]);
 
-// const one = 'Naharis';
-// const two = 'Mormont';
-// const three = 'Sand';
-// console.log(`${one[2]}${two[1]}${three[3]}${two[4]}${two[2]}`);
+//let acces = age > 10 ? 'Its good': 'Not good';
+// if (age >= 10){
+//   acces = "Its good";
+// } else {
+//   acces = "No good"
+// }
+//document.write(`<h3>${acces}</h3>`);
 
-const company1 = 'Apple';
-const company2 = 'Samsung';
-//const companyLength = length(company1);
+// let num = 10;
+// while (num >= 0) {
+//   document.write(`<h4>${num}</h4>`);
+//   num --;
+//   if( num == 3) {
+//     break;
+//   }
+// }
 
-//console.log(companyLength);
-console.log(company1.length + company2.length);
-//console.log(`${company1.length}` '+' `${company2.length}`);
- 
+let num = 0;
+let age = prompt('How old are you?');
+while (true) {
+  if (age > 0 && age < 100) {
+    document.write('<h1>Hello it,s good !</h1>')
+    break;
+  } else {
+    age = prompt('inkorrect input');
+    document.write('<h1>Goodbye!</h1>');
+  }
+
+}
