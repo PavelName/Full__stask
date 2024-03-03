e078aea64fd511add34ee565ee614a8d46120168
Pavel Koryagin <pavel.koryagin.76@bk.ru>
2024-03-03 19:22:54 +0800

    mofified script

--- script.js
+++ script.js
@@ -1,26 +1,10 @@
-const person = {
-  firstName: 'Pavel',
-  lastName: 'Koryagin',
-  age: 47,
-  city:'Usolie-Sib',
-  married: true,
-  languages: {
-    english: 'b3',
-    franse: 'b2',
-  }
-};
-// person.age = 48;
-// console.log(person.age);
-// person.earOfBirth = 1976;
-// console.log(person.earOfBirth);
-// console.log(person.firstName);
-//console.log(person.languages.franse, person.firstName);
-// for(let key in person){
-//   console.log(person[k]);
+// const age = prompt('How old are you?');
+// function isOld(age){
+//   if(age < 10){
+//     return document.write('<h2>It,s no good</h2>')
+//   }
+//   return document.write('<h2>It,s good</h2>')
 // }
-const array = [1, 3, 6, 31, 89];
-// for (let num of array){
-//   console.log(num);
-// }
-array.push(9)
-console.log(array)
+// const send = isOld(age);
+// alert(send);
+console.log(Math.round(Math.random()* 10));
