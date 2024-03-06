8df547cc16b9534965f9bad071af1ed0673ae5c2
Pavel Koryagin <pavel.koryagin.76@bk.ru>
2024-03-06 11:10:29 +0800

    Slider

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-// const age = prompt('How old are you?');
-// function isOld(age){
-//   if(age < 10){
-//     return document.write('<h2>It,s no good</h2>')
-//   }
-//   return document.write('<h2>It,s good</h2>')
-// }
-// const send = isOld(age);
-// alert(send);
-console.log(Math.round(Math.random()* 10));
+const img = document.querySelector('.slider__image');
+
+
+const imgArr = ['./imajes/american-football.jpg', './imajes/caucasian-professional-female-runner.jpg', './imajes/piln.jpg', './imajes/Weight.jpg'];
+
+let currentIndex = 0;
+
+function slide(direction) {
+  currentIndex += direction;
+
+  img.src = imgArr[currentIndex];
+}
