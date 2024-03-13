50530503d63b69c7256f220cfec38a6a1c08fb72
Pavel Koryagin <pavel.koryagin.76@bk.ru>
2024-03-13 17:21:26 +0800

    commit

--- script.js
+++ script.js
@@ -100,6 +100,40 @@ function setActiveClass (arr, index) {
   arr[index].classList.add('active');
 }}
 
+// javascript code for select
+
+const btns = document.querySelectorAll('.filter-btns__item');
+const images = document.querySelectorAll('.filter-content img');
+const select = document.getElementById('filter-options');
+
+for (let btn of btns) {
+  btn.onclick = function ()  {
+    filterImage(this.dataset.filter);
+  }
+}
+
+function filterImage(sel){
+  for(let img of images) {
+    if (!img.classList.contains(sel)) {
+      img.style.display = 'none';
+    } else {
+      img.style.display = 'block';
+    }
+  }
+  for(let btn of btns) {
+    if(btn.dataset.filter === sel) {
+      btn.classList.add('active');
+    }else{
+      btn.classList.remove('active');
+    }
+  }
+  select.value = sel;
+}
+select.addEventListener('change', () => {
+  filterImage(select.value);
+});
+
+
 // training js.
 
 // const getHiddenCard = (numb, star = 4) => {
